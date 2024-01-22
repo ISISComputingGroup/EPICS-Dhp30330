76c1cc90b534c2f5cf07dcd6f1df71e73f8b16a9
Zsolt Kebel <25386639+zsoltkebel@users.noreply.github.com>
2024-01-22 13:41:20 +0000

    Remove PREV pv sets from reset

--- system_tests/tests/dhp30330.py
+++ system_tests/tests/dhp30330.py
@@ -60,9 +60,6 @@ class Dhp30330Tests(unittest.TestCase):
         else:
             self._lewis.backdoor_run_function_on_device("reset")
 
-        self.ca.set_pv_value("CURR:PREV", 0)
-        self.ca.set_pv_value("VOLT:PREV", 0)
-
         time.sleep(5) # Wait for all periodic scan PVs to process.
 
     def _set(self, pv, value):
